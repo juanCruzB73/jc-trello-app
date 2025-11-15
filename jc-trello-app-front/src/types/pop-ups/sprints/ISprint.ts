f65f2e8a8efcaf9c0fc311cf68a1d0048d0be88b
checorazza <ceciicorazza@gmail.com>
2025-11-15 19:54:13 -0300

    add sprint description to tell you how much i love you <3

--- jc-trello-app-front/src/types/pop-ups/sprints/ISprint.ts
+++ jc-trello-app-front/src/types/pop-ups/sprints/ISprint.ts
@@ -3,6 +3,7 @@ import { Itask } from "./ITask";
 export interface ISprint{
     _id?:string;
     title:string;
+    description?:string;
     beginLine:string;
     deadLine:string;
     tasks:Itask[];
