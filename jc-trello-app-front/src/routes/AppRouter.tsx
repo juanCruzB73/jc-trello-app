7b8a4c744ed2101998589eafd8b89dcb27d5a707
checorazza <ceciicorazza@gmail.com>
2026-01-02 19:04:58 -0300

    create auth route

--- jc-trello-app-front/src/routes/AppRouter.tsx
+++ jc-trello-app-front/src/routes/AppRouter.tsx
@@ -2,15 +2,17 @@ import { Navigate, Route, Routes } from "react-router-dom"
 import { BackogsScreen } from "../components/screen/backlog-screen/BackogsScreen"
 import { TaskScreen } from "../components/screen/task-screen/TaskScreen"
 import { sprintStore } from "../store/SprintStore";
+import { AuthScreen } from "../components/screen/auth-screen/AuthScreen";
 
 export const AppRouter = () => {
   const activeSprint = sprintStore((state) => (state.activeSprint));
   
   return (
     <Routes>
-          <Route path="/*" element={<BackogsScreen/>}/>
+          <Route path="/*" element={<AuthScreen/>}/>
           <Route path="/backlogs" element={<BackogsScreen/>}/>
           <Route path="/tasks" element={<TaskScreen/>}/>
+          <Route path="/auth" element={<AuthScreen/>}/>
     </Routes>
   )
 }
